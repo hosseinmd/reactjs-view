65d0aa816fb3fd691190206abd56501379e4ebe4
Hossein Mohammadi <hosseinm.developer@gmail.com>
2022-08-28 18:39:19 +0430

    feat: add exclude to test checkPersian

--- src/tests/src/persianChars.ts
+++ src/tests/src/persianChars.ts
@@ -1,80 +1,72 @@
 import fs from "fs";
 import path from "path";
 
-function readRoot(resolver: (...dirs: string[]) => string) {
-  return fs.readdirSync(resolver(""));
-}
-
 function recursiveChecker(
   componentDir: string,
   resolver: (...dirs: string[]) => string,
+  isIgnore: (dir: string) => boolean,
 ) {
-  const files = fs.readdirSync(resolver(componentDir));
-  files.forEach((file) => {
-    if (fs.lstatSync(resolver(componentDir, file)).isDirectory()) {
-      if (
-        file === "__tests__" ||
-        file === "__mocks__" ||
-        file === "__snapshots__"
-      ) {
-        return;
-      }
-
-      recursiveChecker(resolver(componentDir, file), resolver);
+  if (fs.lstatSync(componentDir).isDirectory()) {
+    if (
+      isIgnore(componentDir) ||
+      componentDir.endsWith("__tests__") ||
+      componentDir.endsWith("__mocks__") ||
+      componentDir.endsWith("__snapshots__")
+    ) {
       return;
     }
-    const regexCodeFiles = /.(ts|js|tsx|jsx)$/;
-
-    if (!regexCodeFiles.test(file)) {
-      return;
-    }
-
-    if (/\.(test|spec)\.(t|j)sx?$/g.test(file) && /\.d\.tsx?$/g.test(file)) {
-      return;
-    }
-
-    const content = fs.readFileSync(resolver(componentDir, file)).toString();
-
-    if (content.split("\n")[0].includes("//ignore-localize")) return;
-
-    content.split("\n").forEach((line, index) => {
-      const matched = line.match(/[آ-ی]/g);
-
-      if (matched) {
-        const error = new Error(
-          `An error occured in ${componentDir
-            .split(/[\\?]/)
-            .join("/")}/${file}:${index + 1}:0 path at line ${line}`,
-        );
-        error.stack = `Error:\nat (${resolver(componentDir, file)}:${
-          index + 1
-        }:0)\n`;
-        test(componentDir, () =>
-          expect(() => {
-            throw error;
-          }).not.toThrow(),
-        );
-      }
+    const files = fs.readdirSync(componentDir);
+    files.forEach((file) => {
+      recursiveChecker(path.join(componentDir, file), resolver, isIgnore);
     });
+
+    return;
+  }
+
+  const regexCodeFiles = /.(ts|js|tsx|jsx)$/;
+
+  if (!regexCodeFiles.test(componentDir)) {
+    return;
+  }
+
+  if (
+    /\.(test|spec)\.(t|j)sx?$/g.test(componentDir) &&
+    /\.d\.tsx?$/g.test(componentDir)
+  ) {
+    return;
+  }
+
+  const content = fs.readFileSync(componentDir).toString();
+
+  if (content.split("\n")[0].includes("//ignore-localize")) return;
+
+  content.split("\n").forEach((line, index) => {
+    const matched = line.match(/[آ-ی]/g);
+
+    if (matched) {
+      const error = new Error(
+        `An error occured in ${componentDir.split(/[\\?]/).join("/")}:${
+          index + 1
+        }:0 path at line ${line}`,
+      );
+      error.stack = `Error:\nat (${componentDir}:${index + 1}:0)\n`;
+      test(componentDir, () =>
+        expect(() => {
+          throw error;
+        }).not.toThrow(),
+      );
+    }
   });
 }
 
-function checkPersian(dirname: string) {
+function checkPersian(dirname: string, exclude: string[] = []) {
   const resolver = (...dirs: string[]) => path.resolve(dirname, `../`, ...dirs);
 
+  const isIgnore = (_dirname: string) =>
+    !!exclude.find((dir) => resolver(dir) === _dirname);
+
   describe("Persian Characters Localizing", () => {
-    const rootDirectories = readRoot(resolver);
-    rootDirectories.forEach((rootDirectory) => {
-      if (fs.lstatSync(resolver(rootDirectory)).isDirectory()) {
-        const componentsDirs = fs.readdirSync(resolver(rootDirectory));
-        componentsDirs.forEach((componentsDir) => {
-          if (fs.lstatSync(resolver(rootDirectory, componentsDir)).isFile()) {
-            return;
-          }
-          recursiveChecker(path.join(rootDirectory, componentsDir), resolver);
-        });
-      }
-    });
+    recursiveChecker(resolver(""), resolver, isIgnore);
 
     it("fake", () => {
       expect(true).toBe(true);
